b72c4063b9441b47404307c59af67a47c5b54be2
Shawnkwiles <48268268+Shawnkwiles@users.noreply.github.com>
2020-08-01 14:45:58 -0500

    Add files via upload

---
+++ Hacker.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        ShowMainMenu();
+
+    }
+
+    void ShowMainMenu() {
+        var greeting = "Hey Jack";
+
+        Terminal.ClearScreen();
+        Terminal.WriteLine(greeting);
+        Terminal.WriteLine("What system will you break?");
+        Terminal.WriteLine("Press 1 for the library");
+        Terminal.WriteLine("Press 2 for the fuzz");
+        Terminal.WriteLine("Enter your selection:");
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
